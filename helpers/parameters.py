f31113367d8cafcde7567df16330fadf75f3f027
nathan getty <nathan.getty@skipthedishes.ca>
2021-05-13 16:22:04 -0500

    argparse and yaml load for config

---
+++ helpers/parameters.py
@@ -0,0 +1,15 @@
+import yaml
+import argparse
+DEFAULT_SETTINGS_FILE = 'settings.json'
+
+
+def load_config(file):
+    with open(file) as file:
+        return yaml.load(file, Loader=yaml.FullLoader)
+
+
+def parse_args():
+    x = argparse.ArgumentParser()
+    x.add_argument('--debug', '-d', help="extra logging", action='store_true')
+    x.add_argument('--config', '-c', help="Path to config.yml")
+    return x.parse_args()
