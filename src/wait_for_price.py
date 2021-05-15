d901b9af3289cff336e31e9d837345356955ebc0
Ivan Dardi <ivanbdardi at gmail.com>
2021-05-15 12:52:05 -0300

    feat: rename variables and format file

--- src/wait_for_price.py
+++ src/wait_for_price.py
@@ -1,50 +1,58 @@
-# This handles price differences and calls the sell_coins function once a threshold
-# has been reached
+"""
+This handles price differences and calls the sell_coins function once a
+threshold has been reached.
+"""
 
-from datetime import datetime, timedelta
 import time
+from datetime import datetime, timedelta
 
-# import local dependencies
-from get_price import get_price
-from sell import sell_coins
-from remove_coins import remove_from_portfolio
 from config import PAIR_WITH, TIME_DIFFERENCE, RECHECK_INTERVAL, CHANGE_IN_PRICE, coins_bought, MAX_COINS
+from get_price import get_price
+from remove_coins import remove_from_portfolio
+from sell import sell_coins
+from src.colors import txcolors
+
 
 def wait_for_price():
-    '''calls the initial price and ensures the correct amount of time has passed
-    before reading the current price again'''
+    """
+    Calls the initial price and ensures the correct amount of time has passed
+    before reading the current price again.
+    """
 
     volatile_coins = {}
-    initial_price = get_price()
+    initial_prices = get_price()
 
-    while initial_price['BNB' + PAIR_WITH]['time'] > datetime.now() - timedelta(seconds=TIME_DIFFERENCE):
-        i=0
+    while initial_prices['BNB' + PAIR_WITH]['time'] > datetime.now() - timedelta(seconds=TIME_DIFFERENCE):
+        i = 0
         while i < RECHECK_INTERVAL:
-            print(f'checking TP/SL...')
+            print(f'Checking TP/SL...')
             coins_sold = sell_coins()
             remove_from_portfolio(coins_sold)
-            time.sleep((TIME_DIFFERENCE/RECHECK_INTERVAL))
+            time.sleep((TIME_DIFFERENCE / RECHECK_INTERVAL))
             i += 1
-            # let's wait here until the time passess...
+            # let's wait here until the time passes...
 
-        print(f'not enough time has passed yet...')
+        print(f'Not enough time has passed yet...')
 
     else:
-        last_price = get_price()
-        infoChange = -100.00
-        infoCoin = 'none'
-        infoStart = 0.00
-        infoStop = 0.00
+        last_prices = get_price()
+        info_change = -100.00
+        info_coin = 'none'
+        info_start = 0.00
+        info_stop = 0.00
 
         # calculate the difference between the first and last price reads
-        for coin in initial_price:
-            threshold_check = (float(last_price[coin]['price']) - float(initial_price[coin]['price'])) / float(initial_price[coin]['price']) * 100
+        for coin in initial_prices:
+            last_price = float(last_prices[coin]['price'])
+            initial_price = float(initial_prices[coin]['price'])
 
-            if threshold_check > infoChange:
-                infoChange = threshold_check
-                infoCoin = coin
-                infoStart = initial_price[coin]['price']
-                infoStop = last_price[coin]['price']
+            threshold_check = (last_price - initial_price) / initial_price * 100
+
+            if threshold_check > info_change:
+                info_change = threshold_check
+                info_coin = coin
+                info_start = initial_price
+                info_stop = last_price
 
             # each coin with higher gains than our CHANGE_IN_PRICE is added to the volatile_coins dict if less than MAX_COINS is not reached.
             if threshold_check > CHANGE_IN_PRICE:
@@ -57,9 +65,9 @@ def wait_for_price():
                     print(f'{txcolors.WARNING}{coin} has gained {threshold_check}% in the last {TIME_DIFFERENCE} seconds, but you are holding max number of coins{txcolors.DEFAULT}')
 
         # Print more info if there are no volatile coins this iteration
-        if infoChange < CHANGE_IN_PRICE:
-                print(f'No coins moved more than {CHANGE_IN_PRICE}% in the last {TIME_DIFFERENCE} second(s)')
+        if info_change < CHANGE_IN_PRICE:
+            print(f'No coins moved more than {CHANGE_IN_PRICE}% in the last {TIME_DIFFERENCE} second(s)')
 
-        print(f'Max movement {float(infoChange):.2f}% by {infoCoin} from {float(infoStart):.4f} to {float(infoStop):.4f}')
+        print(f'Max movement {float(info_change):.2f}% by {info_coin} from {float(info_start):.4f} to {float(info_stop):.4f}')
 
-        return volatile_coins, len(volatile_coins), last_price
+        return volatile_coins, len(volatile_coins), last_prices
