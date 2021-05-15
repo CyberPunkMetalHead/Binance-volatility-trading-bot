3bc0f9dd8896fbf64685e39f3e2e9753c6213140
Ivan Dardi <ivanbdardi at gmail.com>
2021-05-15 12:52:48 -0300

    chore: rename variables and format file

--- src/sell.py
+++ src/sell.py
@@ -1,30 +1,30 @@
-# This module handles the sell logic of our bot
+"""
+This module handles the sell logic of our bot.
+"""
 
-# Import dependencies
-from get_price import get_price
-from config import coins_bought, client, TRAILING_TAKE_PROFIT, TRAILING_STOP_LOSS, USE_TRAILING_STOP_LOSS, LOG_TRADES, TESTNET
 from colors import txcolors
+from config import coins_bought, client, TRAILING_TAKE_PROFIT, TRAILING_STOP_LOSS, USE_TRAILING_STOP_LOSS, LOG_TRADES, TESTNET
+from get_price import get_price
 from save_trade import write_log
 
 
 def sell_coins():
-    '''sell coins that have reached the STOP LOSS or TAKE PROFIT threshold'''
+    """Sell coins that have reached the STOP LOSS or TAKE PROFIT threshold."""
 
-    last_price = get_price()
+    current_prices = get_price()
     coins_sold = {}
 
     for coin in list(coins_bought):
+        last_price = float(current_prices[coin]['price'])
+        buy_price = float(coins_bought[coin]['bought_at'])
+        price_change = float((last_price - buy_price) / buy_price * 100)
+
         # define stop loss and take profit
-        TP = float(coins_bought[coin]['bought_at']) + (float(coins_bought[coin]['bought_at']) * coins_bought[coin]['take_profit']) / 100
-        SL = float(coins_bought[coin]['bought_at']) + (float(coins_bought[coin]['bought_at']) * coins_bought[coin]['stop_loss']) / 100
-
-
-        LastPrice = float(last_price[coin]['price'])
-        BuyPrice = float(coins_bought[coin]['bought_at'])
-        PriceChange = float((LastPrice - BuyPrice) / BuyPrice * 100)
+        TP = buy_price + (buy_price * coins_bought[coin]['take_profit']) / 100
+        SL = buy_price + (buy_price * coins_bought[coin]['stop_loss']) / 100
 
         # check that the price is above the take profit and readjust SL and TP accordingly if trialing stop loss used
-        if float(last_price[coin]['price']) > TP and USE_TRAILING_STOP_LOSS:
+        if last_price > TP and USE_TRAILING_STOP_LOSS:
             print("TP reached, adjusting TP and SL accordingly to lock-in profit")
 
             # increasing TP by TRAILING_TAKE_PROFIT (essentially next time to readjust SL)
@@ -34,10 +34,10 @@ def sell_coins():
             continue
 
         # check that the price is below the stop loss or above take profit (if trailing stop loss not used) and sell if this is the case
-        if float(last_price[coin]['price']) < SL or (float(last_price[coin]['price']) > TP and not USE_TRAILING_STOP_LOSS):
-            print(f"{txcolors.SELL}TP or SL reached, selling {coins_bought[coin]['volume']} {coin} - {BuyPrice} - {LastPrice} : {PriceChange:.2f}%{txcolors.DEFAULT}")
+        if last_price < SL or last_price > TP:
+            print(f"{txcolors.SELL}TP or SL reached, selling {coins_bought[coin]['volume']} {coin} - {buy_price} - {last_price} : {price_change:.2f}%{txcolors.DEFAULT}")
 
-            if TESTNET :
+            if TESTNET:
                 # create test order before pushing an actual order
                 test_order = client.create_test_order(symbol=coin, side='SELL', type='MARKET', quantity=coins_bought[coin]['volume'])
 
@@ -45,10 +45,10 @@ def sell_coins():
             try:
 
                 sell_coins_limit = client.create_order(
-                    symbol = coin,
-                    side = 'SELL',
-                    type = 'MARKET',
-                    quantity = coins_bought[coin]['volume']
+                    symbol=coin,
+                    side='SELL',
+                    type='MARKET',
+                    quantity=coins_bought[coin]['volume']
 
                 )
 
@@ -62,11 +62,12 @@ def sell_coins():
                 # Log trade
 
                 if LOG_TRADES:
-                    profit = (LastPrice - BuyPrice) * coins_sold[coin]['volume']
-                    write_log(f"Sell: {coins_sold[coin]['volume']} {coin} - {BuyPrice} - {LastPrice} Profit: {profit:.2f} {PriceChange:.2f}%")
+                    profit = (last_price - buy_price) * coins_sold[coin]['volume']
+                    write_log(f"Sell: {coins_sold[coin]['volume']} {coin} - {buy_price} - {last_price} Profit: {profit:.2f} {price_change:.2f}%")
+
             continue
 
         # no action
-        print(f'TP or SL not yet reached, not selling {coin} for now {BuyPrice} - {LastPrice} : {PriceChange:.2f}% ')
+        print(f'TP or SL not yet reached, not selling {coin} for now {buy_price} - {last_price} : {price_change:.2f}% ')
 
     return coins_sold
