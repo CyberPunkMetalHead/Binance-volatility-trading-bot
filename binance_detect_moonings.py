3bd864704d3c4d8d9a39d739f30043a1efbbd253
Ivan Dardi <ivanbdardi at gmail.com>
2021-05-15 12:37:16 -0300

    fix: isolate program into a main function

--- binance_detect_moonings.py
+++ binance_detect_moonings.py
@@ -1,15 +1,16 @@
-# The main modules that executes the script repeatedly
+from src.remove_coins import remove_from_portfolio
+from src.sell import sell_coins
+from src.trade import buy
+from src.update_portfolio import update_portfolio
 
-# import local dependencies
-from trade import buy
-from sell import sell_coins
-from update_portfolio import update_portfolio
-from remove_coins import remove_from_portfolio
 
-if __name__ == '__main__':
-
+def main():
     while True:
         orders, last_price, volume = buy()
         update_portfolio(orders, last_price, volume)
         coins_sold = sell_coins()
         remove_from_portfolio(coins_sold)
+
+
+if __name__ == '__main__':
+    main()
