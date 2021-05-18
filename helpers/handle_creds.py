0af306709cf2d86901f04faf2a5603dd91e3cdfc
nathan getty <nathan.getty@skipthedishes.ca>
2021-05-18 12:50:37 -0500

    fixed f string, and added formatting

--- helpers/handle_creds.py
+++ helpers/handle_creds.py
@@ -21,17 +21,22 @@ def test_api_key(client, BinanceAPIException):
     
     except BinanceAPIException as e:   
         if e.code == -2015:
-            return False, "Your API key is not formatted correctly..."
+            msg = "Your API key is not formatted correctly..."
+
         elif e.code == -2014:
             america = "If you are in america, you will have to update your TLD to use 'us'."
             ip_b = "If you set an IP block on your keys make sure this IP address is allowed. check ipinfo.io/ip"
-            return False, "Your API key is either incorrect, IP blocked, or incorrect tld/permissons...\n  {america}\n  {ip_b}"
+            msg = f"Your API key is either incorrect, IP blocked, or incorrect tld/permissons...\n  {america}\n  {ip_b}"
+
         elif e.code == -2021:
             issue = "https://github.com/CyberPunkMetalHead/Binance-volatility-trading-bot/issues/28"
-            msg = "Ensure your OS is time synced with a timeserver. See issue."
-            return False, f"Timestamp for this request was 1000ms ahead of the server's time.\n  {issue}\n  {msg}"
+            desc = "Ensure your OS is time synced with a timeserver. See issue."
+            msg = f"Timestamp for this request was 1000ms ahead of the server's time.\n  {issue}\n  {desc}"
+        
         else:
-            return False, e
+            msg = e
+
+        return False, msg
     
     except Exception as e:
         return False, f"Fallback exception occured:\n{e}"
