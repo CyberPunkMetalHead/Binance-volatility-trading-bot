f6f459731bc3c453dca1f457c2e78fac538cf514
nathan getty <nathan.getty@skipthedishes.ca>
2021-05-18 13:00:15 -0500

    semantics

--- helpers/handle_creds.py
+++ helpers/handle_creds.py
@@ -20,13 +20,16 @@ def test_api_key(client, BinanceAPIException):
         return True, "API key validated succesfully"
     
     except BinanceAPIException as e:   
+    
+      
         if e.code == -2015:
             msg = "Your API key is not formatted correctly..."
 
         elif e.code == -2014:
             america = "If you are in america, you will have to update your TLD to use 'us'."
             ip_b = "If you set an IP block on your keys make sure this IP address is allowed. check ipinfo.io/ip"
-            msg = f"Your API key is either incorrect, IP blocked, or incorrect tld/permissons...\n  {america}\n  {ip_b}"
+            bad_key = "Your API key is not formatted correctly..."
+            msg = f"Your API key is either incorrect, IP blocked, or incorrect tld/permissons...\n  most likely: {bad_key}\n  {america}\n  {ip_b}"
 
         elif e.code == -2021:
             issue = "https://github.com/CyberPunkMetalHead/Binance-volatility-trading-bot/issues/28"
