5e7dcec1512a94548350a38d19e8136961ac8127
nathan getty <nathan.getty@skipthedishes.ca>
2021-06-08 19:40:35 -0500

    Fixes the issue where users get a read timeout.

--- Binance Detect Moonings.py
+++ Binance Detect Moonings.py
@@ -33,6 +33,7 @@ init()
 # needed for the binance API / websockets / Exception handling
 from binance.client import Client
 from binance.exceptions import BinanceAPIException
+from requests.exceptions import ReadTimeout
 
 # used for dates
 from datetime import date, datetime, timedelta
@@ -589,8 +590,13 @@ if __name__ == '__main__':
 
     # seed initial prices
     get_price()
+    READ_TIMEOUT_COUNT=0
     while True:
-        orders, last_price, volume = buy()
-        update_portfolio(orders, last_price, volume)
-        coins_sold = sell_coins()
-        remove_from_portfolio(coins_sold)
+        try:
+            orders, last_price, volume = buy()
+            update_portfolio(orders, last_price, volume)
+            coins_sold = sell_coins()
+            remove_from_portfolio(coins_sold)
+        except ReadTimeout as rt:
+            READ_TIMEOUT_COUNT += 1
+            print(f'We got a timeout error from from binance. Going to re-loop. Current Count: {READ_TIMEOUT_COUNT}')
